76b5aacf4cbcec5bc0e8e11cd52242f24a99d132
Karaś Kacper <kkaras@acram.eu>
2025-04-03 21:59:29 +0200

    Inicializing java project

---
+++ java project/language/src/main/java/com/cmash/Main.java
@@ -0,0 +1,30 @@
+package com.cmash;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception 
+    {
+        // Read the input source code
+        CharStream input = CharStreams.fromFileName (args[0]);
+        
+        // Create the lexer and parser
+        CmashLexer lexer = new CmashLexer (input);
+        CommonTokenStream tokens = new CommonTokenStream (lexer);
+        CmashParser parser = new CmashParser (tokens);
+        
+        // (Optional) Set up error listeners here
+        
+        // Parse starting from 'program'
+        ParseTree tree = parser.program ();
+        
+        // Walk the parse tree using a custom listener or visitor
+        //ParseTreeWalker walker = new ParseTreeWalker ();
+        //CmashCustomListener listener = new CmashCustomListener ();
+        //walker.walk (listener, tree);
+
+        CmashCustomVisitor visitor = new CmashCustomVisitor();
+        visitor.visit(tree);
+    }
+}
